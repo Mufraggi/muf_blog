0540a3af0b5ecdc2a0088d9851d523eee9e0fd66
hugomufraggi <hugo.mufraggi@epitech.eu>
2025-04-18 16:17:32 +0200

    try to add plausible

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,18 @@
     <NuxtPage/>
   </NuxtLayout>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      defer: true,
+      'data-domain': 'mufraggi.eu',
+      src: 'http://plausible-d8skcoc8wowccsskk8g4wsgc.195.201.113.227.sslip.io/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js',
+    },
+    {
+      children: `window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`
+    }
+  ]
+})
+</script>
